2aeeba8cfc16c27cb49c53ea34813aa2d3d7cdea
Eric Finlay <enfinlay@gmail.com>
2015-01-26 15:28:42 -0800

    test - sendStatTest - removed fluff from previous use
    Signed-off-by: Eric Finlay <enfinlay@gmail.com>

--- test/sendStatTest.js
+++ test/sendStatTest.js
@@ -1,3 +1,6 @@
+/*jslint node: true */
+"use strict";
+
 var http = require("http");
 
 // Fixed list of stat names for populating the db
@@ -6,22 +9,23 @@ var statNames = [ "kills", "shots", "deaths", "points", "score", "jumps", "dista
 
 var counter = 0;
 
+// Generate usernames and stats with 50 ms delay to prevent UID collision
+
 var intervalID = setInterval( function() {
 	
 	if(counter >= 1000) {
 		clearInterval(intervalID);
 		return;
 	}
+	
 	callSendStat("Username"+Math.floor(counter/10),statNames[counter % 10]);
 	console.log("Username"+Math.floor(counter/10));
 	console.log("StatName: "+counter% 10);
 	counter++;
 	
-},50)
+},50);
 
-// Modified old call
-// TODO: Make appropriate for this use
-
+//Uncomment to test one entry
 //callSendStat("Billy","jumps");
 
 function callSendStat(userName,statName) {
@@ -35,43 +39,30 @@ function callSendStat(userName,statName) {
 	};
 	
 	var req = http.request(options, function(res) {
-	  //console.log('STATUS: ' + res.statusCode);
-	  //console.log('HEADERS: ' + JSON.stringify(res.headers));
-	  res.setEncoding('utf8');
-	  var responseData = '';
-	  var responseDataJSON = '';
-	  res.on('data', function (chunk) {
-	  	responseData += chunk.toString();
-	    //console.log('BODY: ' + chunk);
-	  });
-	  
-	  res.on('end', function() {
-	  	try {
-	  		JSONObject = JSON.parse(responseData);
-	  	}
-	  	catch (e) {
-	  		console.log('Parse JSON error: '+e);
-	  		return;
-	  	}
-	  	if (JSONObject.error) {
-	  		console.log('Current Rank Error');
-	  		return;
-	  	}
-	  	if (!JSONObject.status || !JSONObject.rank || !JSONObject.prize) {
-	  		console.log('Current Rank missing fields');
-	  		return;
-	  	}
-	  	console.log('Okay');
-	  });
+		
+		res.setEncoding('utf8');
+		var responseData = '';
+
+		res.on('data', function (chunk) {
+			responseData += chunk.toString();
+		});
+
+		res.on('end', function() {
+			console.log(responseData);
+		});
 	});
 	
 	req.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
+		console.log('problem with request: ' + e.message);
 	});
 	
 	// write data to request body
+	var requestBody = { userName: userName,
+		statName: statName,
+		statValue: Math.floor(Math.random()*100) 
+	};
 	
-	req.write('{ "userName": "'+userName+'",  "statName": "'+statName+'","statValue": '+Math.floor(Math.random()*100)+'}');
+	req.write(JSON.stringify(requestBody));
 	req.end();
 }
 	
